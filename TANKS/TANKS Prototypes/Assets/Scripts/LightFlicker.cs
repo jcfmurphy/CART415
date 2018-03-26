1b29a32f755b876e3372f67fad9d7c4d3d20785e
jcfmurphy <jcfmurphy@gmail.com>
2018-03-26 19:21:17 -0400

    Added flickering main light to Miserables

---
+++ TANKS/TANKS Prototypes/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+	private Light m_Light;
+	private Color m_OriginalColor;
+	private float m_NoiseScale = 1f;
+
+	// Use this for initialization
+	void Start () {
+		m_Light = GetComponent<Light> ();
+		m_OriginalColor = m_Light.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = 1f + ((Mathf.PerlinNoise (Time.time * m_NoiseScale, 0f) - 0.5f) * 0.5f);
+
+		m_Light.color = m_OriginalColor * offset;
+	}
+}
